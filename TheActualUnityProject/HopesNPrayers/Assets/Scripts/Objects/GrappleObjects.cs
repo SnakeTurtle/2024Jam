48455990886afe3b3fd8c869d894f5ee3ee525a8
Codeathan <jarj2205@gmail.com>
2024-08-17 19:28:40 -0400

    Setup test Commit
    Setup

--- TheActualUnityProject/HopesNPrayers/Assets/Scripts/Objects/GrappleObjects.cs
+++ TheActualUnityProject/HopesNPrayers/Assets/Scripts/Objects/GrappleObjects.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour
+public class GrappleObjects : MonoBehaviour
 {
+    public DistanceJoint2D grapple;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grapple = GetComponent<DistanceJoint2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //grapple.connectedBody
     }
 }
