f166eef0e3f8ed1c17d418d9aec4f81a082da5de
JoshuanBarba-NJIT <joshuanbarba04@gmail.com>
2024-08-17 19:49:12 -0400

    AI

--- TheActualUnityProject/HopesNPrayers/Assets/Enemy_AI.cs
+++ TheActualUnityProject/HopesNPrayers/Assets/Enemy_AI.cs
@@ -6,23 +6,39 @@ public class Enemy_AI : MonoBehaviour
 {
 
     public float viewDistance;
+    GameObject eyeBalls;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        eyeBalls = gameObject.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, viewDistance);
+        FindPlayer();
+        GetBigger();
+    }
+
+    private void FindPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(eyeBalls.transform.position, Vector2.right, viewDistance);
         Debug.Log(hit.collider.gameObject.name);
         if (hit.collider.gameObject.tag == "Player")
         {
             Debug.Log("PlayerFound");
         }
     }
+    private void GetBigger()
+    {
+        bool a = Input.GetKeyDown(KeyCode.F);
+        if (a)
+        {
+            gameObject.transform.localScale = new Vector3(3, 4, 3);
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
